a45228279b70d810f1d792ddf4e6d19926c52dcb
Juan Marcos Cruz Melara <melaratechlab@gmail.com>
2024-11-17 10:06:54 -0600

    The simple game was done!

--- ChooseTheNumber/ChooseTheNumber/main.swift
+++ ChooseTheNumber/ChooseTheNumber/main.swift
@@ -7,5 +7,43 @@
 
 import Foundation
 
-print("Hello, World!")
+var SecretNumber: Int = Int.random(in: 1...100)
+var numberOfTries: Int = 0
 
+print("I'm thinking of a number between 1 and 100. Can you guess it?")
+
+while true {
+    let guess: Int = ReadUserNumber()
+    
+    if guess == SecretNumber {
+        print("You got it! The number was \(SecretNumber).")
+        print("You take \(numberOfTries) tries.")
+        break
+    }else{
+        numberPosition(SecretNumber, guess: guess)
+        numberOfTries += 1
+    }
+}
+
+func ReadUserNumber() -> Int {
+    if let userNumber = readLine(strippingNewline: true), let number = Int(userNumber) {
+        if !(0...100).contains(number) {
+            print("The number must be between 0 and 100.")
+            return ReadUserNumber()
+        }else{
+            return number
+        }
+    }else{
+        print("The number must be an integer.")
+        return ReadUserNumber()
+    }
+}
+
+func numberPosition(_ number: Int, guess: Int){
+    if guess < number {
+        print("Your election is bellow from the special number. Try again!")
+    }else{
+        print("Your election is above from the special number. Try again!")
+    }
+}
+    
